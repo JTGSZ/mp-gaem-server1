c8ad6410a1b989764053ede912f07b736118cde1
JTGSZ <46565256+JTGSZ@users.noreply.github.com>
2022-10-24 17:45:39 -0400

    1

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -4,8 +4,8 @@ import { ArcadePhysics } from "arcade-physics";
 import { Body } from 'arcade-physics/lib/physics/arcade/Body';
 
 const FPS = 60;
-const CAT_WIDTH = 100;
-const CAT_HEIGHT = 200;
+const CAT_WIDTH = 32;
+const CAT_HEIGHT = 32;
 const CAT_SPEED = 200;
 
 export class MyRoom extends Room<MyRoomState> {
@@ -13,11 +13,13 @@ export class MyRoom extends Room<MyRoomState> {
 	physics: ArcadePhysics = null;
 	tick: number = 0;
 	bodies: Record<string, Body> = {};
+	collisiongroup: Array<Body> = []
 
   	onCreate(options: any) {
     	this.setState(new MyRoomState());
+		this.collisiongroup = []
 
-		this.onMessage("input", (client, { up, left, down, right }) => {
+		this.onMessage("input", (client, { up, left, down, right, attack }) => {
 			const body = this.bodies[client.sessionId];
 
 			if (up) {
@@ -39,72 +41,80 @@ export class MyRoom extends Room<MyRoomState> {
 			else {
 				body.setVelocityX(0);
 			}
+
+      		if(attack){
+				//TODO cram a hitbox in here and make it do somethin
+      		}
     	});
 
-    // Initialize the room's physics
-    const config = {
-      sys: {
-        game: {
-          config: {}
-        },
-        settings: {
-          	physics: {
-            	debug: true,
-				gravity: {
-					x: 0,
-					y: 0
-            	}
-          	}
-        },
-        scale: {
-          width: 2400 * 2,
-          height: 1200
-        },
-        queueDepthSort: () => {}
-      }
-    };
+		// Initialize the room's physics
+		const config = {
+		sys: {
+			game: {
+			config: {}
+			},
+			settings: {
+				physics: {
+					debug: true,
+					gravity: {
+						x: 0,
+						y: 0
+					}
+				}
+			},
+			scale: {
+			width: 2400 * 2,
+			height: 1200
+			},
+			queueDepthSort: () => {}
+		}
+		};
 
-    this.physics = new ArcadePhysics(config);
+    	this.physics = new ArcadePhysics(config);
+		this.physics.add.collider(this.collisiongroup, null, null, null, null)
 
-    this.setSimulationInterval((deltaTime) => this.update(deltaTime));
-  }
+    	this.setSimulationInterval((deltaTime) => this.update(deltaTime));
+  	}
 
-  update(deltaTime: number) {
-    this.physics.world.update(this.tick * 1000, 1000 / FPS);
-    this.tick++;
+	update(deltaTime: number) {
+		this.physics.world.update(this.tick * 1000, 1000 / FPS);
+		this.tick++;
 
-    this.syncState();
-  }
+		this.syncState();
+	}
 
-  syncState() {
-    // Loop over all the players in the room, and sync their X/Ys with that of their physics body
-    this.state.players.forEach((player, sessionId) => {
-      	const body = this.bodies[sessionId];
+	syncState() {
+		// Loop over all the players in the room, and sync their X/Ys with that of their physics body
+		this.state.players.forEach((player, sessionId) => {
+			const body = this.bodies[sessionId];
 
-      	player.x = body.x;
-      	player.y = body.y;
-    });
-  }
+			player.x = body.x;
+			player.y = body.y;
+		});
+	}
 
-  onJoin(client: Client, options: any) {
-    console.log(client.sessionId, "joined!");
+	onJoin(client: Client, options: any) {
+		console.log(client.sessionId, "joined!");
 
-    // When a player joins the room, assign them both a state representation and a physics body
-    this.state.players.set(client.sessionId, new Player(0, 0));
-    this.bodies[client.sessionId] = this.physics.add.body(0, 0, CAT_WIDTH, CAT_HEIGHT);
-  }
+		// When a player joins the room, assign them both a state representation and a physics body
+		this.state.players.set(client.sessionId, new Player(0, 0));
+		let body = this.physics.add.body(0, 0, CAT_WIDTH, CAT_HEIGHT);
+		this.bodies[client.sessionId] = body
+		this.collisiongroup.push(body)
+		
+	}
 
-  onLeave(client: Client, consented: boolean) {
-    console.log(client.sessionId, "left!");
+	onLeave(client: Client, consented: boolean) {
+		console.log(client.sessionId, "left!");
 
-    // When a player leaves the room, delete their state representation and physics body
-    this.state.players.delete(client.sessionId);
-    this.bodies[client.sessionId].destroy();
-    delete this.bodies[client.sessionId];
-  }
+		// When a player leaves the room, delete their state representation and physics body
+		this.state.players.delete(client.sessionId);
+		this.bodies[client.sessionId].destroy();
+		delete this.bodies[client.sessionId];
+	}
 
-  onDispose() {
-    console.log("room", this.roomId, "disposing...");
-  }
+	onDispose() {
+		console.log("room", this.roomId, "disposing...");
+	}
 
 }
